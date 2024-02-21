658b5baa2af8b339ca0dc8a7d8fdc1e62aa88d4b
ocahs9 <piorag99@gmail.com>
2024-02-22 00:05:41 +0900

    just commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Home from "./routes/Home";
 function App()
 { //url 변수명은 중복되어서는 안됨
   return (
-    <Router>
-      <Routes>
-        <Route path="/hello" element={<h1>Hello~</h1>}>
+    <Router> 
+      <Routes basename={process.env.PUBLIC_URL}>
+        <Route path={`${process.env.PUBLIC_URL}/hello`} element={<h1>Hello~</h1>}>
         </Route>
-        <Route path="/movie/:id/:idd" element={<Detail/>}> 
+        <Route path={`${process.env.PUBLIC_URL}/movie/:id/:idd`} element={<Detail/>}> 
         </Route>
-        <Route path="/" element={<Home/>}> 
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home/>}> 
         </Route>
       </Routes> 
     </Router>
